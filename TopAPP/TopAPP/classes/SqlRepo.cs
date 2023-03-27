4c4fe36f0ef50fe53878890abf5c4eae976c3f72
minecrecz <Dvorak.Adam@stud.vassboskovice.cz>
2023-03-27 18:36:41 +0200

    práce na SqlRepo a Users

--- TopAPP/TopAPP/classes/SqlRepo.cs
+++ TopAPP/TopAPP/classes/SqlRepo.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TopAPP.classes;
 
 namespace TopAPP
 {
-    internal class SqlRepo
+    public class SqlRepo
     {
+        private string connectionString = @"Data Source=vps.jhlavacek.cz;Initial Catalog=Dvorak;User ID=DvorakAdam;Password=********;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        public List<User> GetUsers()
+        {
+            List<User> users = new List<User>();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                using (SqlCommand cmd = sqlConnection.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Users";
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var user = new User(reader["Name"].ToString()
+                                             , (byte[])reader["PasswordHash"]
+                                             , (byte[])reader["PasswordSalt"]
+                                             , (string)reader["Rights"]
+                                             );
+                            users.Add(user);
+                        }
+                    }
+                }
+                sqlConnection.Close();
+            }
+            return users;
+        }
+
     }
 }
