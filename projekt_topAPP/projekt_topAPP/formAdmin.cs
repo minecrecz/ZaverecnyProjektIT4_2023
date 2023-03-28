bd5bea50f378b7612cc2b1e703fb7075bf23ebcc
minecrecz <90258732+minecrecz@users.noreply.github.com>
2023-03-28 21:20:12 +0200

    jsem opice a neumím C# 😢

---
+++ projekt_topAPP/projekt_topAPP/formAdmin.cs
@@ -0,0 +1,37 @@
+﻿using projekt_topAPP.třídy;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projekt_topAPP
+{
+    public partial class formAdmin : Form
+        
+    {
+        
+        private sql_repository sql_repository;
+        private Employee employee;
+        private List<string[]> employees;
+        
+        public formAdmin()
+        {
+            InitializeComponent();
+            sql_repository = new sql_repository();
+        }
+
+        private void formAdmin_Load(object sender, EventArgs e)
+        {
+            foreach (var row in sql_repository.GetEmployees)
+            {
+                listViewEmployee.Items.Add(new ListViewItem(row));
+            }
+        }
+        
+    }
+}
---
+++ projekt_topAPP/projekt_topAPP/třídy/sql repository.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using projekt_topAPP.třídy;
+
+namespace projekt_topAPP
+{
+    public class sql_repository
+    {
+        
+        private string connectionString = @"Data Source=vps.jhlavacek.cz;Initial Catalog=Dvorak;User ID=DvorakAdam;Password=********;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        /*private List<User> GetUsers()
+        {
+            List<User> users = new List<User>();
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from User";
+                    using(SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var user =new User(reader["Name"].ToString()
+                                , (byte[])reader["PasswordHash"]
+                                , (byte[])reader["passwordSalt"], 
+                                reader["Rights"].ToString())
+                                ;
+                            users.Add(user);
+                        }
+                        
+                        
+                        
+                    }
+                }
+                conn.Close();
+            }
+            return users;
+        }/*
+      public List<User> GetUsers(string searchString)
+       {
+           List<User> users = new List<User>();
+           using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+           {
+               sqlConnection.Open();
+               using (SqlCommand cmd = sqlConnection.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Users where Name like @Search";
+                    cmd.Parameters.AddWithValue("Search", "%" + searchString + "%");
+                   using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                      {
+                           var user = new User(reader["Name"].ToString()
+                                             , (byte[])reader["PasswordHash"]
+                                             , (byte[])reader["PasswordSalt"])
+                               
+                              ;
+                            
+                           users.Add(user);
+                        }
+                    }
+                }
+                sqlConnection.Close();
+           }
+            return users;
+        }
+        public User? GetUser(string username)
+        {
+            User? user = null;
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+           {
+                sqlConnection.Open();
+                using (SqlCommand cmd = sqlConnection.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Users where Name=@Name";
+                    cmd.Parameters.AddWithValue("Name", username);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                       {
+                            user = new User(reader["Name"].ToString(), (byte[])reader["PasswordHash"], (byte[])reader["PasswordSalt"])
+                                
+                                ;
+                            
+                                               }
+                    }
+                }
+                sqlConnection.Close();
+            }
+           return user;
+        }
+        public void ConvertUsersToHashed()
+        {
+            var users = GetUsers();
+            foreach (var user in users)
+            {
+                
+            }
+        }*/
+         public List<string[]> GetEmployees()
+        {
+            List<string[]> employees = new List<string[]>();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                
+                using (SqlCommand cmd = sqlConnection.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT FirstName,Forename,BirthDate,Email,Phone FROM Employee";
+                    sqlConnection.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            string[] row  = new string[] { reader[0].ToString(), reader[1].ToString(), reader[2].ToString(), reader[3].ToString() };
+                           employees.Add(row);
+                        }
+                    }
+                }
+                sqlConnection.Close();
+            }
+            return employees;
+        }
+        
+           
+    }
+}
